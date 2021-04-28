e71814e3f5b3723e49eb86e72d1a09dc7bb77714
Adron4444 <adron4444@gmail.com>
2021-04-28 22:33:01 +0200

    Create simple port scanner

--- port_scanner.rb
+++ port_scanner.rb
@@ -1,1 +1,14 @@
-puts("Port 22 is open in 192.168.1.101")
+require "socket"
+
+PORT = ARGV[0] || 22
+
+HOST = ARGV[1] || 'localhost'
+
+begin
+  socket = TCPSocket.new(HOST, PORT)
+  status = 'open'
+rescue Errno::ECONNREFUSED, Errno::ETIMEDOUT
+  status = 'close'
+end
+
+puts("Port #{PORT} is #{status} in #{HOST}")
